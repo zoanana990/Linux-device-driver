456f2f254b9df87526a9c41efc734e05310f9aba
zoanana990 <harry021633@gmail.com>
2023-03-01 19:54:17 +0800

    Add file operation function

--- 02_pseudo_char_driver/main.c
+++ 02_pseudo_char_driver/main.c
@@ -3,6 +3,9 @@
 #include <linux/cdev.h>
 #include <linux/device.h>
 #include <linux/kdev_t.h>
+#include <linux/uaccess.h>
+
+/* MACRO DEFINITION */
 #define DEV_MEM_SIZE 512
 
 /* Add module description */
@@ -21,26 +24,95 @@ struct cdev pcd_cdev;
 
 loff_t pcd_lseek(struct file *filp, loff_t off, int whence)
 {
-    return 0;
+    loff_t temp;
+
+    pr_info("lseek requested\n");
+    pr_info("Current value of the file position = %lld\n", filp->f_pos);
+    
+    switch(whence)
+    {
+        case SEEK_SET:
+            if((offset > DEV_MEM_SIZE) || (offset < 0))
+                return -EINVAL;
+            filp->f_pos = offset;
+            break;
+        case SEEK_CUR:
+            temp = filp->f_pos + offset;
+            if((temp > DEV_MEM_SIZE) || (temp < 0))
+                return -EINVAL;
+            filp->f_pos = temp;
+            break;
+        case SEEK_END:
+            temp = DEV_MEM_SIZE + offset;
+            if((temp > DEV_MEM_SIZE) || (temp < 0))
+                return -EINVAL;
+            filp->f_pos = temp;
+            break;
+        default:
+            return -EINVAL;
+    }
+
+    pr_info("New Value of the file position = %lld\n", filp->f_pos);
+    return filp->f_pos;
 }
 
 ssize_t pcd_read(struct file * filp, char __user *buff, size_t count, loff_t *f_pos)
 {
-    return 0;
+    /* current file position */
+    pr_info("Current file position = %lld\n", *f_pos);
+
+    /* adjust count, if it is bigger than the memory size */
+    if((*f_pos + count) > DEV_MEM_SIZE)
+        count = DEV_MEM_SIZE - *f_ops;
+
+    /* copy the data to the user space */
+    if(copy_to_user(buff, &device_buffer[*f_pos], count))
+        return -EFAULT;
+
+    /* update the current file position */
+    *f_pos += count;
+
+    pr_info("Number of bytes successfully read = %zu\n", count);
+    pr_info("Updated file position = %lld\n", *f_pos);
+
+    return count;
 }
 
 ssize_t pcd_write(struct file *filp, const char __user *buff, size_t count, loff_t *f_pos)
 {
-    return 0;
+    /* current file position */
+    pr_info("Current file position = %lld\n", *f_pos);
+
+    /* adjust count, if it is bigger than the memory size */
+    if((*f_pos + count) > DEV_MEM_SIZE)
+        count = DEV_MEM_SIZE - *f_ops;
+    
+    if(!count)
+        return -ENOMEM;
+
+    /* copy the data to the user space */
+    if(copy_from_user(&device_buffer[*f_pos], buff, count))
+        return -EFAULT;
+
+    /* update the current file position */
+    *f_pos += count;
+
+    pr_info("Number of bytes successfully written = %zu\n", count);
+    pr_info("Updated file position = %lld\n", *f_pos);
+
+    return count;
 }
 
+/* This driver is pseudo device, thus we don't need to implement it */
 int pcd_open(struct inode *inode, struct file *filp)
 {
+    pr_info("Open was successful\n");
     return 0;
 }
 
 int pcd_release(struct inode *inode, struct file *filp)
 {
+    pr_info("Close was successful\n");
     return 0;
 }
 
