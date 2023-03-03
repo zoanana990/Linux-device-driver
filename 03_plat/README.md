4d247c4b86a14e73d323025e8b3dc17ba56d5c6f
zoanana990 <harry021633@gmail.com>
2023-03-03 19:20:16 +0800

    Add platfrom driver prototype

---
+++ 03_plat/README.md
@@ -0,0 +1,3 @@
+# pseudo platform device driver
+This project is used to regist a platform device and platform driver
+
---
+++ 03_plat/pcd_device_setup.c
@@ -0,0 +1,8 @@
+/* This project is aims to match the platform driver by the platform bus 
+ * 1. create two platform devices
+ * 2. 
+ */
+
+#include <linux/module.h>
+
+
