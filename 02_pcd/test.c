f58e6b10c515f6130bdee8a71ac3f7483ac13d97
zoanana990 <harry021633@gmail.com>
2023-03-04 17:46:09 +0800

    Modify the test script

--- 02_pcd/test.c
+++ 02_pcd/test.c
@@ -1,82 +1,67 @@
-#include <sys/types.h> 
-#include <sys/stat.h> 
+#include <pthread.h>
 #include <fcntl.h>
-#include<unistd.h>
-#include<errno.h>
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
-char buffer[2048];
+#define MAX_STRING_LENGTH 	128
 
-#define TRY_READ 10
+#define EXPECT(exp)								\
+    {										\
+	if(!(exp))								\
+	    frpintf(stderr, "FAILURE @ %s:%u; %s\n", __FILE__, __LINE__, #exp); \
+	abort();								\
+    }
+
+/* This test script is used to test the psuedo character device behavior, 
+ * Due to multiple device nodes, we use multithread to test here
+ */
+static int remaining = 10;
+static char buffer[2048];
+
+bool EXAM(char *device_name)
+{
+    /* fd  for file descriptior,
+     * ret for the return value
+     */
+     
+    int fd, ret; 
+
+    /* open a pcd */
+    fd = open("%s", device_name, O_RDWR);    
+    
+    if(fd < 0)
+    {
+	perror("open");
+	return false;
+    }   
+
+    /* TODO: try to write the device value */    
+
+    ret = write(&fd, "Hello World", 12);
+
+    /* try to read the device value */
+    ret = read(&fd, &buffer[0], MAX_STRING_LENGTH);
+
+    /* TODO: compare the value is equivalent */
+
+
+    /* show the value */
+    for(int i=0 ; i < MAX_STRING_LENGTH ; i++)
+	printf("%c",buffer[i]);
+
+
+    /* test complete, clse the pcd */
+    close(fd);
+}
+
 
 int main(int argc, char *argv[])
 {
-	int fd;
+
+    EXAM("/dev/pcdev-3");
 	
-	/*this variable holds remaining data bytes to be read */
-	int remaining = TRY_READ;
-	
-	/*Holds count of total data bytes read so far */
-	int total_read=0;
-	
-	int n =0,ret=0;
-
-	if(argc != 2 ){
-		printf("Wrong usage\n");
-		printf("Correct usage: <file> <readcount>\n");
-		return 0;
-	}
-
-
-	/*convert command line supplied data to integer */
-	remaining = atoi(argv[1]);
-
-	printf("read requested = %d\n",remaining);
-
-
-	fd = open("/dev/pcdev-0",O_RDONLY);
-
-	if(fd < 0){
-		/*perror decodes user space errno variable and prints cause of failure*/
-		perror("open");
-		return fd;
-	}
-
-	printf("open was successful\n");
-	/*Lets attempt reading twice */
-	
-	while(n != 2 && remaining)
-	{
-		/*read data from 'fd' */
-		ret = read(fd,&buffer[total_read],remaining);
-
-		if(!ret){
-			/*There is nothing to read */
-			printf("end of file \n");
-			break;
-		}else if(ret <= remaining){
-			printf("read %d bytes of data \n",ret );
-			/*'ret' contains count of data bytes successfully read , so add it to 'total_read' */
-		        total_read += ret;
-			/*We read some data, so decrement 'remaining'*/
-			remaining -= ret;
-		}else if(ret < 0){
-			printf("something went wrong\n");
-			break;
-		}else
-			break;
-
-		n++;
-	}
-
-	printf("total_read = %d\n",total_read);
-
-	//dump buffer
-	for(int i=0 ; i < total_read ; i++)
-		printf("%c",buffer[i]);
-
-	close(fd);
-	
-	return 0;
+    return 0;
 }
+
